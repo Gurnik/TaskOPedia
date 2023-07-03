f3a2a163c3db1047c23fe16922b5edd803d8896b
Daniel Kandalaft <kandddany1985@gmail.com>
2023-07-03 19:37:26 +0300

    Student Component

--- src/index.js
+++ src/index.js
@@ -44,9 +44,34 @@ function MainBody() {
         <li>Basic Foudation</li>
         <li>Functional and Class Components</li>
       </ul>
+      {/*
       <div>
         Enter Task :{" "}
         <input maxLength={5} readOnly={false} placeholder="Ben"></input>
+      </div> 
+  */}
+    </div>
+  );
+}
+
+function Students() {
+  const fullName = "Kris Walley";
+  const programmingExp = 2;
+  return (
+    <div className="container p-4">
+      <div className="row">Students Enrolled</div>
+      <div className="row border">
+        <div className="col-2">
+          <img
+            src={`https://ui-avatars.com/api/?name=${fullName}`}
+            className="w-100"
+          ></img>
+        </div>
+        <div className="col-10">
+          {fullName}
+          <br />
+          Coding Experience {programmingExp} years
+        </div>
       </div>
     </div>
   );
@@ -62,6 +87,7 @@ root.render(
   <div>
     <Header />
     <MainBody />
+    <Students />
     <Footer />
   </div>
 );
