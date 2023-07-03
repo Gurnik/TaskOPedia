faa90a135808961f903ae6f9a0584bd11d1d39f0
Daniel Kandalaft <kandddany1985@gmail.com>
2023-07-03 16:58:57 +0300

    Assignment 3 - Adding Const

--- src/index.js
+++ src/index.js
@@ -31,11 +31,14 @@ function MainBody() {
   const whatWeWillLearn = "React JS";
   // let whatWeWillLearn = "React JS";
   // var whatWeWillLearn = "React JS"; typically not used
+  const lectureCount = 3;
 
   return (
     <div>
       <p>
         In this course, we will learn {whatWeWillLearn} by building TaskOPedia!
+        <br />
+        Total Lecture - {lectureCount}
       </p>
       <ul>
         <li>Basic Foudation</li>
